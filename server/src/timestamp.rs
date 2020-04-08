e20440fe1f4914cd600c191bf8b91c074eb19460
Jane Lusby <jlusby@yaah.dev>
2020-04-07 18:25:07 -0700

    remove a dbg

--- server/src/timestamp.rs
+++ server/src/timestamp.rs
@@ -7,7 +7,7 @@ impl std::str::FromStr for ReportTimestamp {
     type Err = std::num::ParseIntError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        Ok(Self(dbg!(input.parse())?))
+        Ok(Self(input.parse()?))
     }
 }
 
